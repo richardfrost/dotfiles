21f13018ca97ca8841248397d4af05d8100ba5d1
Richard Frost <richard.frost@mx.com>
2017-04-06 12:24:24 -0600

    Upate path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:$DOTFILES/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
