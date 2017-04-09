12adb32e40bf3277a03f377ecbb3be918fad0c9b
Richard Frost <richard.frost@mx.com>
2017-04-09 11:53:53 -0600

    Add system ruby gems bin to path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,7 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+# Add system ruby bin to path
+if which ruby >/dev/null && which gem >/dev/null; then
+  export PATH="$PATH:$(ruby -rubygems -e 'puts Gem.user_dir')/bin"
+fi
