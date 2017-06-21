9130d9f7c15be96399c19d2f2e09c6682e6e0b64
Richard Frost <richard.frost@mx.com>
2017-06-21 11:18:29 -0600

    Switch from rbenv through brew to git clone

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,4 +1,4 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.rbenv/bin:$DOTFILES/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
 # Add system ruby bin to path
