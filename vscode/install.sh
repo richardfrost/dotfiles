c2771339982b2e49355b96c8bb026a9343a8e0c4
Richard Frost <richard.frost@mx.com>
2017-10-23 15:45:36 -0600

    Initial VSCode settings

---
+++ vscode/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Windows %APPDATA%\Code\User\settings.json
+# Mac $HOME/Library/Application Support/Code/User/settings.json
+# Linux $HOME/.config/Code/User/settings.json
+
+# Link Sublime Text 3 User configs
+if [ "$(uname -s)" = "Darwin" ]
+then
+  echo "› Setup VSCode Config for MacOS"
+  ln -s "$DOTFILES/vscode/User" "$HOME/Library/Application Support/Code/User"
+else
+  echo "› Setup VSCode Config for Linux"
+  echo "TODO: Find destination path"
+fi
