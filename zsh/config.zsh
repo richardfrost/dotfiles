9e798eb18269f868a5cf9299b268105db9bd6e63
Richard Frost <richard@richardfrost.info>
2021-04-27 17:34:15 -0600

    :sparkles: Improve history in bash/zsh

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -6,29 +6,30 @@ fpath=($DOTFILES/functions $fpath)
 autoload -U $DOTFILES/functions/*(:t)
 
 # History
-HISTCONTROL=ignoredups:ignorespace
-HISTTIMEFORMAT="%s "
 HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=100000
+SAVEHIST=100000
+setopt HIST_IGNORE_SPACE # Don't write commands that begin with a space to history
+setopt APPEND_HISTORY # Don't overwrite history file
+# setopt INC_APPEND_HISTORY # Adds history incrementally
+setopt SHARE_HISTORY # Share history across sessions
+setopt HIST_IGNORE_ALL_DUPS  # Don't record dupes in history
+setopt HIST_REDUCE_BLANKS # Remove superfluous blanks from each command line
+setopt EXTENDED_HISTORY # Add timestamps to history
+setopt HIST_SAVE_NO_DUPS # Older duplicate commands are omitted
+# setopt HIST_VERIFY # Show command with history expansion to user before running it
 
+# Other options
 setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
 setopt NO_LIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
 setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
-setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 setopt CORRECT
 setopt COMPLETE_IN_WORD
 # setopt IGNORE_EOF # logoff with CTRL+D
-
-setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
-setopt HIST_REDUCE_BLANKS
 
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
