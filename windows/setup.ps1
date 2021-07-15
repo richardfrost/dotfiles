9856e601e39ca8c093ea88919ea4488a9d25a4d5
Richard Frost <richard@richardfrost.info>
2021-07-15 15:49:27 -0600

    :sparkles: Add a starting point for Windows setup

---
+++ windows/setup.ps1
@@ -0,0 +1,51 @@
+function Invoke-MirrorFolder {
+  Param
+  (
+    [String]$Source,
+    [String]$Destination
+  )
+
+  Robocopy $Source $Destination /MIR /FFT /Z /XA:H /W:5
+}
+
+function New-Symlink {
+  Param (
+    [String]$Link,
+    [String]$Target
+  )
+
+  Write-Host "Creating Symlink: ${Link} => ${Target}"
+}
+
+function Set-UserEnvVar {
+  Param
+  (
+    [String]$Name,
+    [String]$Value
+  )
+
+  Write-Host "Setting User EnvVar: ${Name} = ${Value}"
+  if ((Get-ChildItem -Path "Env:\$Name").Value -ne $Value) {
+    [System.Environment]::SetEnvironmentVariable($Name, $Value, [System.EnvironmentVariableTarget]::User)
+  }
+}
+
+## Setup System
+# Variables
+$SyncHomePath = 'D:\OneDrive'
+$AppsPath = "$SyncHomePath\Apps"
+
+## Environment Variables
+Set-UserEnvVar -Name 'SyncHome' -Value $SyncHomePath
+Set-UserEnvVar -Name 'Apps' -Value $AppsPath
+
+## Symlinks
+# ! Stored in OneDrive now
+# New-Symlink -Link $env:USERPROFILE\Documents\WindowsPowerShell\ -Target
+
+## Copy Files
+# Copy Shortcuts
+# C:\Users\phros\AppData\Roaming\Microsoft\Windows\Start Menu\Programs
+#
+Write-Host "Copying shortcut files to Start Menu..."
+Invoke-MirrorFolder -Source "$AppsPath\Windows\Shortcuts" -Destination "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Shortcuts"
