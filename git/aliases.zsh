7e439d769d3f9215f13cc134b4c7e80936726211
Richard Frost <richard@richardfrost.info>
2022-02-17 10:36:41 -0700

    :hammer: Add rebase aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -20,3 +20,5 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
+alias grebase='git rebase -i --ignore-date'
+alias grebaseabort='git rebase --abort'
