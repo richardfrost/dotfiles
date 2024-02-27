514bb37e4628f6d58eebf88b86d3f335e5300afb
Richard Frost <richard@richardfrost.info>
2024-02-27 11:24:01 -0700

    :wastebasket: Don't run softwareupdate on macOS

--- macos/install.sh
+++ macos/install.sh
@@ -5,7 +5,7 @@
 # command line interface to it that we can use to just install everything, so
 # yeah, let's do that.
 
-if [[ "$(uname -s)" == "Darwin" ]]; then
-  echo "› sudo softwareupdate -i -a"
-  sudo softwareupdate -i -a
-fi
+# if [[ "$(uname -s)" == "Darwin" ]]; then
+#   echo "› sudo softwareupdate -i -a"
+#   sudo softwareupdate -i -a
+# fi
