431e9a263a4893b1120a07872573400905510b61
Richard Frost <richard@richardfrost.info>
2024-02-27 11:16:05 -0700

    :wastebasket: Deprecate Homebrew further

--- _deprecated/homebrew/install.sh
+++ _deprecated/homebrew/install.sh
@@ -12,6 +12,27 @@
 #   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
 # fi
 
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  read -e -p "Set macOS Defaults?: (y/n) " setMacOSDefaults
+  if [[ "${setMacOSDefaults}" == "y" || "${setMacOSDefaults}" == 'Y' ]]; then
+    echo "› OSX defaults"
+    "$DOTFILES/macos/set-defaults.sh"
+  fi
+
+  read -e -p "Install Homebrew?: (y/n) " installHomebrew
+  if [[ "${installHomebrew}" == "y" || "${installHomebrew}" == 'Y' ]]; then
+    # Install homebrew
+    "$DOTFILES/homebrew/install.sh" 2>&1
+
+    # Upgrade homebrew
+    echo "› Brew"
+    brew update
+    brew upgrade
+    "$DOTFILES/homebrew/links.sh" 2>&1
+    "$DOTFILES/homebrew/services.sh" 2>&1
+  fi
+fi
+
 # TODO: Check for Homebrew
 # Install Homebrew only for macOS
 if [[ "$(uname -s)" == "Darwin" ]]; then
